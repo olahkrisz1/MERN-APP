ea606a876cc428eed28f4938d80633cb594fe8af
olahkrisz1 <olahkrisz1@gmail.com>
2023-07-01 20:51:57 +0200

    register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const [user, setUser] = React.useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const register = () =>{
+        console.log(user)
+    }
+
   return (
-  <div className="flex justify-center h-screen items-center bg-green-500">
-    <h1 className="text text-3xl">
-        Register
-    </h1>
+    <div className="flex justify-center h-screen items-center bg-primary">
+      <div className="bg-white p-5 w-[450px]">
+        <div className="flex flex-col gap-5">
+        <h1 className="text-2xl font-bold text-center text-primary uppercase">Matrix Master Community - Register</h1>
+        <input type="text" placeholder="Enter your name"
+        value={user.name}
+        onChange={(e) => setUser({...user, name: e.target.value})} 
+        />
+
+        <input type="email" placeholder="Enter your email"
+        value={user.email}
+        onChange={(e) => setUser({...user, email: e.target.value})} 
+        />
+
+        <input type="password" placeholder="Enter your password"
+        value={user.password}
+        onChange={(e) => setUser({...user, password: e.target.value})} 
+        />  
+        
+        <button className="primary-contained-btn"
+        onClick={register}
+        >Register</button>
+        
+        <Link to="/login" className="text-center text-primary underline">Already have an account? Login</Link>
+        </div>
+
+
+      </div>
     </div>
     )
 };
