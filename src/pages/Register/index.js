4ead7865531b038a5ebaf56885a985dd62c89f8b
olahkrisz1 <olahkrisz1@gmail.com>
2023-07-01 20:10:18 +0200

    setup, folders, tailwind css added

---
+++ src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+  return (
+  <div className="flex justify-center h-screen items-center bg-green-500">
+    <h1 className="text text-3xl">
+        Register
+    </h1>
+    </div>
+    )
+};
+
+export default Register;
